8c3be9a0746f744b2f4fc5ac23eeac5de6f721b6
thomasmendonca <rdcharacter10@gmail.com>
2024-03-21 19:17:34 -0300

    Criando página Github page

---
+++ CP01/Controllers/GitHubPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CP01.Controllers
+{
+    public class GitHubPageController : Controller
+    {
+        public IActionResult GitHubPage()
+        {
+            return View();
+        }
+    }
+}
