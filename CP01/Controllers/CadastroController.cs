16283cc93140df07d85819423cd1c972d946fa74
thomasmendonca <rdcharacter10@gmail.com>
2024-03-21 18:40:52 -0300

    Add project files.

---
+++ CP01/Controllers/CadastroController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CP01.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            ViewBag.textoCadastro = "Cadastro";
+            return View();
+        }
+    }
+}
---
+++ CP01/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CP01.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            ViewBag.textoLogin = "Login";
+            return View();
+        }
+    }
+}
